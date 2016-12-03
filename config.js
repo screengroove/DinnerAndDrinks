62df3e6a1eebf4a80dee34a575fe5c1960c7ef73
Justin Biele <justin.biele@gmail.com>
2016-12-03 13:17:02 -0800

    [refactor] Refactor db schemas with successful save to db

--- config.js
+++ config.js
@@ -8,6 +8,10 @@ module.exports = {
   yelpConsumerKey: '9JsyXXhj9pxI0wRtxIdb9Q',
   yelpConsumerSecret: '0D3FHzK5wuysal4WYZY3CeYEdR8',
   yelpToken: 'ivBw983_ASe9DysxtuqI7cxlrGBFOI4m',
-  yelpTokenSecret: 'k6QLpFpCtW0r3j_DsqJKEgqmI-Q'
+  yelpTokenSecret: 'k6QLpFpCtW0r3j_DsqJKEgqmI-Q',
+  'database': {
+    'test': 'mongodb://localhost:27017/test',
+    'mongo': 'mongodb://beeple:secwets@ds011228.mlab.com:11228/beeple'
+  }
 }
 
--- database/db.js
+++ database/db.js
@@ -3,53 +3,56 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 // Schema
-let schema = new Schema({
-  user: {
-    auth: {
-      username: String,
-      email: String,
-      password: String,
-      fbAuth: Boolean
-    },
-    profile: [{
-      name: String,
-      bio: String,
-      photo: String,
-      hometown: String,
-      interests: String
-    }],
-    favoites: [{
-      yelpId: String,
-      name: String,
-      catergories: [String],
-      address: String,
-      phone: String,
-      rating: Number,
-      image_url: String,
-      bussinessUrl: String,
-      lat: Number,
-      long: Number
-    }]
+let userSchema = new Schema({
+  auth: {
+    username: String,
+    email: String,
+    password: String,
+    fbAuth: Boolean
   },
-  comments: [{
-    author: String,
-    message: String,
-    date: Date,
-    venue: String
-  }],
-  hotspots: [{
+  profile: [{
     name: String,
+    bio: String,
+    photo: String,
+    hometown: String,
+    interests: String
+  }],
+  favoites: [{
+    yelpId: String,
+    name: String,
+    catergories: [String],
     address: String,
-    latitude: Number,
-    longitude: Number,
-    description: String,
+    phone: String,
     rating: Number,
-    image: String
+    image_url: String,
+    bussinessUrl: String,
+    lat: Number,
+    long: Number
   }]
+})
+const commentSchema = new Schema({
+  author: String,
+  message: String,
+  date: Date,
+  venue: String
+})
+const hotspotSchema = new Schema({
+  name: String,
+  address: String,
+  latitude: Number,
+  longitude: Number,
+  description: String,
+  rating: Number,
+  image: String
 })
 
 // Models
-let Rec = mongoose.model('Rec', schema)
-
+let User = mongoose.model('User', userSchema)
+let Hotspot = mongoose.model('Hotspot', hotspotSchema)
+let Comment = mongoose.model('Review', commentSchema)
 // Exports here
-module.exports = Rec
+module.exports = {
+  User: User,
+  Hotspot: Hotspot,
+  Comment: Comment
+}
