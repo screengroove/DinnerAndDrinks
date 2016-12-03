2ffd4ba02a59fda6b398293a10a88905f9df9e33
laynefaler <laynefaler@gmail.com>
2016-12-03 11:33:05 -0800

    [pull] added

--- public/bundle.js
+++ public/bundle.js
@@ -26730,25 +26730,25 @@
 	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 	var YelpMap = function (_React$Component) {
-	  _inherits(YelpMap, _React$Component);
+	    _inherits(YelpMap, _React$Component);
 
-	  function YelpMap() {
-	    _classCallCheck(this, YelpMap);
+	    function YelpMap() {
+	        _classCallCheck(this, YelpMap);
 
-	    return _possibleConstructorReturn(this, (YelpMap.__proto__ || Object.getPrototypeOf(YelpMap)).apply(this, arguments));
-	  }
-
-	  _createClass(YelpMap, [{
-	    key: 'getYelpMap',
-	    value: function getYelpMap() {}
-	  }, {
-	    key: 'render',
-	    value: function render() {
-	      return _react2.default.createElement('div', null);
+	        return _possibleConstructorReturn(this, (YelpMap.__proto__ || Object.getPrototypeOf(YelpMap)).apply(this, arguments));
 	    }
-	  }]);
 
-	  return YelpMap;
+	    _createClass(YelpMap, [{
+	        key: 'getYelpMap',
+	        value: function getYelpMap() {}
+	    }, {
+	        key: 'render',
+	        value: function render() {
+	            return _react2.default.createElement('div', null);
+	        }
+	    }]);
+
+	    return YelpMap;
 	}(_react2.default.Component);
 
 	exports.default = YelpMap;
@@ -28251,7 +28251,7 @@
 	'use strict';
 
 	Object.defineProperty(exports, "__esModule", {
-	  value: true
+	    value: true
 	});
 
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
