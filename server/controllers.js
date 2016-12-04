77965abaceaf9fbc9cc73fe6560de80f14be77e2
laynefaler <laynefaler@gmail.com>
2016-12-03 17:33:37 -0800

    Search get and post

--- server/controllers.js
+++ server/controllers.js
@@ -1,4 +1,5 @@
 // call model functions in controller functions
+const yelp = require('./config')
 const models = require('./models')
 
 module.exports = {
@@ -47,9 +48,44 @@ module.exports = {
   },
   yelp: {
     get: (req, res) => {
-           
+      
     },
     post: (req, res) => {
+            /* can look something like this
+                models.user.post() */
+    },
+    getSearch: (req, res) => {
+      yelp.search({
+        latitude: 0.0,
+        longitude: 0.0,
+        term: '',
+        sort: 0,
+        category_filter: '',
+        catergories: '',
+        rating: 0
+      })
+      .then(resp => {
+        res.send(resp)
+      }).catch(err => { console.log(`getSearch Yelp error ${err}`) })
+    },
+    postSearch: (req, res) => {
+      yelp.search({
+        latitude: req.body.latitude,
+        longitude: req.body.longitude,
+        term: req.body.term,
+        sort: req.body.sort,
+        category_filter: req.body.catergory_filter,
+        catergories: req.body.catergories,
+        rating: req.body.rating
+      })
+      .then(resp => {
+        res.send(resp)
+      }).catch(err => { console.log(`getSearch Yelp error ${err}`) })
+    },
+    getBusiness: (req, res) => {
+      
+    },
+    postBusiness: (req, res) => {
             /* can look something like this
                 models.user.post() */
     }
--- server/routes.js
+++ server/routes.js
@@ -27,6 +27,10 @@ router.post('/maps', controllers.maps.post)
 // yelp
 router.get('/yelp', controllers.yelp.get)
 router.post('/yelp', controllers.yelp.post)
+router.get('/yelp/search', controllers.yelp.getSearch)
+router.post('/yelp/search', controllers.yelp.postSearch)
+router.get('/yelp/business', controllers.yelp.getBusiness)
+router.post('/yelp/business', controllers.yelp.postBusiness)
 
 // export router for server.js
 module.exports = router
