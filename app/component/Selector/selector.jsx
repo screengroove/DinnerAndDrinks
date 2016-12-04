573041e0737ff494f203c7688ba53f906ee62e8c
laynefaler <laynefaler@gmail.com>
2016-12-03 21:47:32 -0800

    [added] react redux

--- app/component/Selector/selector.jsx
+++ app/component/Selector/selector.jsx
@@ -2,13 +2,11 @@ import React from 'react'
 
 export default class Selector extends React.Component {
 
-  getCoffee () { return 'coffee' }
-  getMovies () { return 'movies' }
-  getRestaurants () { return 'restaurants' }
-
   render () {
     return (
-      <div />
+        <div>
+          
+        </div>
     )
   }
 }
--- app/pages/MainPage/mainPage.jsx
+++ app/pages/MainPage/mainPage.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 
 // importing map view
 import MapView from './../../component/MapView/mapView.jsx'
-import YelpMap from './../../component/YelpMap/yelpMap.jsx'
 import HotspotForm from './../../component/Hotspot/hotspotForm.jsx'
 import HotspotList from './../../component/Hotspot/hotspotList.jsx'
 
