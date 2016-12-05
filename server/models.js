e66d7f74179cec43c1877ef5e377eb7b89799ddd
Justin Biele <justin.biele@gmail.com>
2016-12-04 22:31:53 -0800

    [merge] Solve merge conflict

--- server/models.js
+++ server/models.js
@@ -56,9 +56,9 @@ module.exports = {
   },
   hotspots: {
     get: (req, res) => {
-      Hotspot.find().exec((err, data) => {
-        console.log('ah ha...data: ', data)
-        return data
+      Hotspot.find().exec( (err, data) => {
+        console.log("ah ha...data: ", data)
+        res.send(data)
       })
     },
     post: (req, res) => {
