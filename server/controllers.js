18d05c080d02b11d80304a2d42f25b74fa6da7a6
Justin Biele <justin.biele@gmail.com>
2016-12-05 15:29:10 -0800

    [edit] Edit coordinate variable name

--- server/controllers.js
+++ server/controllers.js
@@ -37,11 +37,7 @@ module.exports = {
                 models.user.post() */
     }
   },
-
-
-
   hotspots: {
-
     get: (req, res) => {
       models.hotspots.get(req.body, res)
 
@@ -49,37 +45,21 @@ module.exports = {
       console.log('this is the req.body: ', req.body)
       models.hotspots.get(req.body, res)
     },
-
     post: (req, res) => {
-      console.log('req.body in the server controller: ', req.body)
-
       req.body.lat=""
       req.body.long=""
-
       request.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + req.body.address + '&key=' + API_KEY, (error, response, body) => {
         if (error) {
           console.log(`There has been a grave error: ${error}`)
         }
-        let poo = JSON.parse(body)
-        req.body.lat = poo.results[0].geometry.location.lat
-        req.body.long = poo.results[0].geometry.location.lng
+        let coordinates = JSON.parse(body)
+        req.body.lat = coordinates.results[0].geometry.location.lat
+        req.body.long = coordinates.results[0].geometry.location.lng
         models.hotspots.post(req.body)
         res.send(req.body)
-
       })
-
     }
-
   },
-
-
-
-
-
-
-
-
-
   yelp: {
     getPhoneSearch: (req, res) => {
       yelp.phoneSearch({ phone: '' })
