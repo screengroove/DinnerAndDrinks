db122884d894ed9897cdcd084a3dcaacdc1d93da
Justin Biele <justin.biele@gmail.com>
2016-12-05 15:29:10 -0800

    [edit] Edit coordinate variable name

--- server/controllers.js
+++ server/controllers.js
@@ -37,17 +37,12 @@ module.exports = {
                 models.user.post() */
     }
   },
-
-
-
   hotspots: {
-
     get: (req, res) => {
       console.log('These are the res: ', res.data)
       console.log('this is the req.body: ', req.body)
       models.hotspots.get(req.body, res)
     },
-
     post: (req, res) => {
       req.body.lat=""
       req.body.long=""
@@ -62,17 +57,7 @@ module.exports = {
         res.send(req.body)
       })
     }
-
   },
-
-
-
-
-
-
-
-
-
   yelp: {
     getPhoneSearch: (req, res) => {
       yelp.phoneSearch({ phone: '' })
