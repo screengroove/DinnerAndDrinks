ac9e51b1249b303b8888ab378ef7af6f6f90a942
laynefaler <laynefaler@gmail.com>
2016-12-06 14:20:18 -0800

    ready for pull

--- app/pages/MasterPage.jsx
+++ app/pages/MasterPage.jsx
@@ -1,19 +1,38 @@
 import React from 'react'
-
-// Needed for Routing only
-// Not a component page or display page
-// anything on this MasterPage will be on EVERY page of the app
-import MainPage from './MainPage/MainPage.jsx'
+import { Router, Route, Link, browserHistory } from 'react-router'
+import AppBar from 'material-ui/AppBar'
+import Drawer from 'material-ui/Drawer'
+import MenuItem from 'material-ui/MenuItem'
 
 export default class MasterPage extends React.Component {
 
   constructor (props) {
     super(props)
+    this.state = {
+      open: false
+    }
+  }
+
+  handleToggle (e) {
+    this.setState({open: !this.state.open})
   }
 
   render () {
     return (
       <div>
+        <AppBar
+            title="Recommendator"
+            onLeftIconButtonTouchTap={this.handleToggle}
+          />
+          {console.log(this.state.open)}
+        <Drawer
+          width={200}
+          open={this.state.open}
+        >
+          <MenuItem>Home</MenuItem>
+          <MenuItem>Sign Up</MenuItem>
+          <MenuItem>Favorites</MenuItem>
+        </Drawer>
         {this.props.children}
       </div>
     )
