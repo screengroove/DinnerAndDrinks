cc4ab6c2a82f027f2a98a30143a3eff05e92c701
Justin Biele <justin.biele@gmail.com>
2016-12-05 19:09:24 -0800

    [merge] Solve merge conflicts

--- app/component/Hotspot/hotspotForm.jsx
+++ app/component/Hotspot/hotspotForm.jsx
@@ -18,7 +18,6 @@ export default class HotspotForm extends React.Component {
     let address = document.getElementById('hotspotAddress').value
     let description = document.getElementById('hotspotDescription').value
 
-
     let location = {
       name: name,
       address: address,
