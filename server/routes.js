d758267c64b86b9f4af8f398a334c80a14a3f5c8
Jimmy Gonzalez <jimmygonzalez87@gmail.com>
2016-12-10 18:39:05 -0800

    [feat] ends points setup

--- server/routes.js
+++ server/routes.js
@@ -2,33 +2,11 @@
 const router = require('express').Router()
 const controllers = require('./controllers')
 
-// api/ server roots
-
-// authentication
-router.get('/auth', controllers.users.get)
-router.post('/auth', controllers.users.post)
-
-// favorites
-router.get('/favorites', controllers.favorites.get)
-router.post('/favorites', controllers.favorites.post)
-router.delete('/favorites', controllers.favorites.delete)
-
-// comments
-// router.get('/comments', controllers.comments.get)
-// router.post('/comments', controllers.comments.post)
-
-// hotspots
-router.get('/hotspots', controllers.hotspots.get)
-router.post('/hotspots', controllers.hotspots.post)
-
-// maps
-router.get('/maps', controllers.maps.get)
-router.post('/maps', controllers.maps.post)
 
 // yelp
 router.get('/yelp/search', controllers.yelp.getSearch)
 router.post('/yelp/search', controllers.yelp.postSearch)
-router.get('/yelp/business', controllers.yelp.getBusiness)
+router.get('/yelp/searchbars', controllers.yelp.getBars)
 router.post('/yelp/business', controllers.yelp.postBusiness)
 
 // export router for server.js
